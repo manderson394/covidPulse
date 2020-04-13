8bdc9e24bfe15a3c566256178ad5ec0b7c60b3e2
Kevin Kelm <kkelm@outlook.com>
2020-04-13 11:11:59 -0500

    Needed to commit a last minute change to all service before I merge master into all service.

--- src/main/java/edu/matc/covidPulse/controller/AllService.java
+++ src/main/java/edu/matc/covidPulse/controller/AllService.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import edu.matc.covidPulse.entity.CountyCovidData;
 import edu.matc.covidPulse.entity.CountyFips;
 import edu.matc.covidPulse.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -19,6 +22,8 @@ public class AllService {
 
     private GenericDao countyFipsDao;
     private GenericDao countyCovidDataDao;
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     public AllService() {  }
 
@@ -35,17 +40,12 @@ public class AllService {
         String json = "";
 
         try {
-
             ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
             json = mapper.writeValueAsString(countyCovidDataList);
-
         }
         catch (JsonProcessingException e) {
-
-            e.printStackTrace();
-
+            logger.error(e.getMessage());
         }
 
         return Response.status(200).entity(json).build();
@@ -53,5 +53,4 @@ public class AllService {
         //return Response.status(200).entity(countyCovidDataList).build();
     }
 
-    // NOTE: not including optional dates here since it would be the same as county data with date constraints.
 }
