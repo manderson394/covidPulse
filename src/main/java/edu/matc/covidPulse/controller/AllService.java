99af2012fc035818a58d069c35f263c7d1c3eff0
Kevin Kelm <kkelm@outlook.com>
2020-04-13 00:41:15 -0500

    Worked on getting all of the data from the COUNTY_COVID_DATA table, and wrote a test. What took so long was that I played around with reconstructing the JSON that was getting passed from the get all DAO method.  (9 Hours)

--- src/main/java/edu/matc/covidPulse/controller/AllService.java
+++ src/main/java/edu/matc/covidPulse/controller/AllService.java
@@ -3,31 +3,24 @@ package edu.matc.covidPulse.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import edu.matc.covidPulse.entity.*;
+import edu.matc.covidPulse.entity.CountyCovidData;
+import edu.matc.covidPulse.entity.CountyFips;
 import edu.matc.covidPulse.persistence.GenericDao;
-import edu.matc.covidPulse.persistence.SessionFactoryProvider;
-import org.hibernate.SessionFactory;
-
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Path("/all")
 @Transactional
 public class AllService {
 
-    private SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
-
     private GenericDao countyFipsDao;
     private GenericDao countyCovidDataDao;
 
     public AllService() {  }
-
 
     @GET
     @Path("/")
@@ -37,23 +30,17 @@ public class AllService {
         countyFipsDao = new GenericDao(CountyFips.class);
         countyCovidDataDao = new GenericDao(CountyCovidData.class);
 
-        List<CountyFips> countyFipsList = countyFipsDao.getAll();
-
-        //List<CountyCovidData> countyCovidDataList = countyCovidDataDao.getById();
-
-        Set<CountyFips> dataSet = new HashSet<>();
-
-        for (CountyFips d : countyFipsList) {
-            dataSet.add(d);
-        }
+        List<CountyCovidData> countyCovidDataList = countyCovidDataDao.getAll();
 
         String json = "";
 
         try {
+
             ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-            json = mapper.writeValueAsString(dataSet);
+            json = mapper.writeValueAsString(countyCovidDataList);
+
         }
         catch (JsonProcessingException e) {
 
@@ -61,8 +48,9 @@ public class AllService {
 
         }
 
-
         return Response.status(200).entity(json).build();
+
+        //return Response.status(200).entity(countyCovidDataList).build();
     }
 
     // NOTE: not including optional dates here since it would be the same as county data with date constraints.
